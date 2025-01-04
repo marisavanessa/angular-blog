9b019efe5cba6be4d0175c89dfb64bac892ef41b
Marisa Vanessa Norberto da Cruz <ideiaselembrancas@gmail.com>
2025-01-04 13:15:38 -0300

    Item 14 Deixando a contentPage Dinâmica

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
+    "id":"1",
     "title": "Nova linha de ônibus",
-    "description": "Terminal Vila Arens - Jardim Bonfiglioli"
+    "description": "Terminal Vila Arens - Jardim Bonfiglioli",
+    "photoCover": "https://noticiando.net/wp-content/uploads/2019/04/Linhas-704-e-974.jpg"
   },
   {
-    "id":1,
-    "photo":"aaa",
+    "id":"2",
     "title": "Nova linha de ônibus",
-    "description": "Terminal Vila Arens - Vila Rami"
+    "description": "Terminal Vila Arens - Vila Rami",
+    "photoCover": "https://tvtecjundiai.com.br/news/wp-content/uploads/sites/2/2019/05/onibus_mobiliade-total-35.jpg"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,9 +8,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit{
-  photoCover:string= "https://jundiai.sp.gov.br/noticias/wp-content/uploads/sites/32/2023/07/expresso-943-eloy-chaves-central.jpg"
-  contentTitle:string= "Com reformulação de linhas, expresso Terminal Eloy Chaves – Terminal Central passa a operar aos sábados"
-  contentDescription:string= "olá mundo!"
+  photoCover:string= ""
+  contentTitle:string= ""
+  contentDescription:string= ""
+  private id:string | null = "0"
 
   constructor(
     private route:ActivatedRoute
@@ -17,9 +19,20 @@ export class ContentComponent implements OnInit{
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
 
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+
+  }
+
 
 }
